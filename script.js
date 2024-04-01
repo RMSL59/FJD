bc46dd852c17ab2964715de1f5c540cafc9e35f4
RMSL59 <remi.salome@hotmail.fr>
2024-04-01 16:05:25 +0100

    Change everything again

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function processInput() {
-    var userInput = document.getElementById('userInput').value;
-    var outputDiv = document.getElementById('output');
-
-    // Example processing: simply displaying the input back to the user
-    outputDiv.innerText = "You entered: " + userInput;
-}
+document.getElementById('goButton').addEventListener('click', function() {
+    var input1Value = document.getElementById('input1').value;
+    var input2Value = document.getElementById('input2').value;
+    
+    // Check if both input boxes are not empty
+    if (input1Value !== '' && input2Value !== '') {
+        alert('Values entered: ' + input1Value + ' and ' + input2Value);
+    } else {
+        alert('Please enter values in both input boxes.');
+    }
+});
