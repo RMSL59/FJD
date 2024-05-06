474bc2ac4821be192445d9b0263d70d0c07e75b2
RMSL59 <remi.salome@hotmail.fr>
2024-05-06 11:30:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('goButton').addEventListener('click', function() {
     var TKValue = document.getElementById('TKNumber').value;
     
     // Check if both input are not equal to the winning numbers
-    if (TicketValue !== '0196035458-DK' && TKValue !== 'TK-ERP-475') {
+    if (TicketValue = '0196035458-DK' && TKValue = 'TK-ERP-475') {
         window.location.replace('winning.html');
     } else {
         error-message.style.display = 'block';
